8c3140a12504434916223390a63df2ac13b32da5
PhilippMDoerner <philipp3000doerner@web.de>
2024-05-11 15:54:56 +0200

    Refinements

--- src/slacktors/serverActor/codegen.nim
+++ src/slacktors/serverActor/codegen.nim
@@ -1,7 +1,7 @@
 import ./serverActorType
 import chronos
 import chronos/threadsync
-import std/[importutils, macros]
+import std/[importutils, sequtils, strutils, strformat, macros]
 
 export importutils
 
@@ -31,6 +31,7 @@ proc generateInternalProcessMessages(types: NimNode): NimNode =
   for typ in types:
     let processMailboxNode = quote do:
       for msg in `server`.sources[`typ`].messages:
+        debug "Bulk recv: Thread <= Mailbox", server = `server`.name, msgTyp = $`typ`, msg = msg
         try:
           `server`.process(msg)
         except KillError as e:
@@ -52,8 +53,6 @@ proc generateInternalHasMessages(types: NimNode): NimNode =
   ##   return false
   ## ```
   result = newProc(params = @[ident("bool")], procType = nnkLambda)
-  # result.addPragma(ident("nimcall"))
-  # result.addPragma(ident("gcsafe"))
   let mailboxes = ident("mailboxes")
   let mailboxesParam = newIdentDefs(
     mailboxes, ident("MailboxTable")
@@ -94,30 +93,83 @@ proc generateMailboxes(types: NimNode, size: NimNode): NimNode =
   mailboxTable.add(mailboxesIdent)
   return newBlockStmt(mailboxTable)
 
+proc validateTypes(types: NimNode) =
+  case types.kind
+  of nnkBracket:
+    for typ in types:
+      let isTypeDesc = typ.kind == nnkSym
+      if not isTypeDesc:
+        error(fmt"""
+          "{typ.repr}" is not a type description, but was found among the input-message types:
+          {types.repr}.
+          It may have been pushed in there due to calling "initServerActor" with parameters that do not have the type you expect. 
+        """.strip())
+  else:
+    error(fmt"""
+      The types passed in to initServerActor were not all of type `typedesc`: 
+      {types.repr}
+    """.strip())
+  
+  
 proc createServerActor(
-  variable: NimNode, 
   mailboxSize: NimNode, 
+  serverName: NimNode, 
   types: NimNode
 ): NimNode =
+  validateTypes(types)
+
   let boxesBlockNode = generateMailboxes(types, mailboxSize)
   let processProcNode = generateInternalProcessMessages(types)
   let hasMessagesProcNode = generateInternalHasMessages(types)
   return quote do:
-    `variable` = 
-      block:
-        privateAccess(ServerActor)
-        let processProc = `processProcNode`
-        let mailboxes = `boxesBlockNode`
-        let hasMessagesProc = `hasMessagesProcNode`
-        ServerActor(
-          name: "unspecified",
-          sources: mailboxes,
-          processProc: processProc,
-          hasMessagesProc: hasMessagesProc,
-          signalReceiver: new(ThreadSignalPtr)[]
-        )
+    block:
+      privateAccess(ServerActor)
+      let processProc = `processProcNode`
+      let mailboxes = `boxesBlockNode`
+      let hasMessagesProc = `hasMessagesProcNode`
+      ServerActor(
+        name: `serverName`,
+        sources: mailboxes,
+        processProc: processProc,
+        hasMessagesProc: hasMessagesProc,
+        signalReceiver: new(ThreadSignalPtr)[]
+      )
 
-macro initServerActor*(variable: var ServerActor, mailboxSize: int, types: varargs[typed]): typed =
-  let x = createServerActor(variable, mailboxSize, types)
-  # echo x.repr
-  return x
+const DEFAULT_SERVER_NAME = "unspecified"
+const DEFAULT_MAILBOX_SIZE = 5
+macro initServerActor*(
+  mailboxSize: int,
+  serverName: string,
+  types: varargs[typed]
+): ServerActor =
+  let actorNode = createServerActor(mailboxSize, serverName, types)
+  when defined(slacktorDebug):
+    echo actorNode.repr
+  return actorNode
+
+macro initServerActor*(
+  mailboxSize: int,
+  types: varargs[typed]
+): ServerActor = createServerActor(
+  mailboxSize, 
+  newStrLitNode(DEFAULT_SERVER_NAME), 
+  types
+)
+
+macro initServerActor*(
+  serverName: string,
+  types: varargs[typed]
+): ServerActor = createServerActor(
+  newIntLitNode(DEFAULT_MAILBOX_SIZE), 
+  serverName, 
+  types
+)
+
+macro initServerActor*(
+  types: varargs[typed]
+): ServerActor = createServerActor(
+  newIntLitNode(DEFAULT_MAILBOX_SIZE), 
+  newStrLitNode(DEFAULT_SERVER_NAME), 
+  types
+)
+  
--- src/slacktors/serverActor/serverActorType.nim
+++ src/slacktors/serverActor/serverActorType.nim
@@ -8,7 +8,7 @@ type ShutdownError* = object of CatchableError ## Indicates an issue that happen
 type SendingError* = object of ValueError ## Thrown when a message was attempted to be sent to a recipient that cannot possibly receive it.
 
 type ServerActor* = object
-  name*: string
+  name: string
   sources: MailboxTable
   targets: MailboxTable
   processProc: proc(server: ServerActor) {.nimcall.} # Processing is done by capturing "process(value: T)" overloads
@@ -19,16 +19,15 @@ proc sendWakeupSignalTo*(server: ServerActor) =
   let response = server.signalReceiver.fireSync()
   let hasSentSignal = response.isOk()
   if not hasSentSignal:
-    notice "Failed to wake up threadServer: ", name = server.name
-
+    notice "Failed to wake up server: ", targetServer = server.name
 
 proc waitForSendSignal*(server: ServerActor) = 
   ## Causes the server to work through its remaining async-work
   ## and go into a low powered state afterwards. Receiving a signal
   ## will wake the server up again.
-  debug "Going to sleep", serverName = server.name
+  debug "Going to sleep", server = server.name
   waitFor server.signalReceiver.wait()
-  debug "Waking up", serverName = server.name
+  debug "Waking up", server = server.name
 
 proc processMessages*(actor: ServerActor) = 
   actor.processProc(actor)
@@ -37,17 +36,17 @@ proc hasMessages*(actor: ServerActor): bool =
   return actor.hasMessagesProc(actor.sources)
 
 proc gracefulShutdown*(server: ServerActor) =
-  notice "Regular shut down of server started", serverName = server.name
+  notice "Regular shut down of server started", server = server.name
   server.processMessages() # Process remaining messages
   let closeResult = server.signalReceiver.close()
   if closeResult.isErr():
     raise newException(ShutdownError, closeResult.error)
-  notice "Regular shut down of server ended", serverName = server.name
+  notice "Regular shut down of server ended", server = server.name
 
 proc sendTo*[T](value: T, server: ServerActor) =
+  debug "trySend  : Thread => Mailbox", server = server.name, msgTyp = $T
   try:
     server.sources[T].send(value)
-
   except KeyError as e:
     raise newException(SendingError, fmt"The server '{server.name}' does not have a mailbox for type '{$T}'. ", parentException = e)
   
@@ -55,6 +54,7 @@ proc sendTo*[T](value: T, server: ServerActor) =
     raise newException(SendingError, "Failed to send message to server " & server.name, parentException = e)
 
 proc trySendTo*[T](value: T, server: ServerActor): bool =
+  debug "trySend  : Thread => Mailbox", server = server.name, msgTyp = $T
   try:
     return server.sources[T].trySend(value)
   
